1d1285bf2d8b4db8214cfbfcbd1b695ac9d48e2b
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-06-27 17:22:41 -0300

    importando html

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-    <div></div>
+    <div>
+
+        <div class="jumbotron jumbotron-fluid"> 
+            <div class="container">  
+                <h1 class="display-4">Lista de Filmes</h1>
+                <p class="lead">Treinamento comunicação entre Componentes no Vue</p>
+            </div>
+        </div>
+        <div class="container"> 
+            <FilmesLista/>
+        </div>
+    </div>
 </template>
+
+<script>
+
+import FilmesLista from './components/FilmesLista';
+
+export default {
+    components: {
+        FilmesLista,
+    }    
+}
+</script>
