6dbb6a760b4b678431521e86f36aabe4b3566d6b
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2020-06-27 12:01:20 -0300

    atributo data retorna uma funcao

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 import Vue from 'vue'
 
+// const data = {
+//   titulo: 'Curso Coppetec',
+//   contador: 0,
+// }
+
+Vue.component('cptec-helloword', {
+  data: function() {
+    return {
+      titulo: 'Curso Coppetec',
+      contador: 0,
+    };
+  },
+  methods: {
+    incrementar: function() {
+      this.contador++;
+    }
+  },
+  template: `
+    <div>
+      <h1> {{titulo}} </h1>
+      <button @click="incrementar">Clicado {{contador}} vezes</button>
+    </div>    
+  `
+});
+
 new Vue({
-  el: '#app',
-  template: '<h1>Olá Mundo!</h1>'
-})
+  el: '#app'
+});
